1275efe6bf993762844c46c5de017df08eeb8700
kushvashisth <kushvashisthkv@gmail.com>
2022-02-10 16:35:43 +0530

    Third Commit

--- models/articles.js
+++ models/articles.js
@@ -7,19 +7,6 @@ const dompurify = createDomPurify(new JSDOM().window);
 
 const Schema = mongoose.Schema;
 
-// const commentSchema = new Schema({
-//     comment:  {
-//         type: String,
-//         required: true
-//     },
-//     author: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User'
-//     }
-// },{
-//     timestamps: true
-// });
-
 const articleSchema = new Schema({
     title: {
         type: String,
@@ -38,7 +25,7 @@ const articleSchema = new Schema({
         type: String,
         required: true
     },
-//    comments:[commentSchema]
+
 }, {
     timestamps: true
 });
